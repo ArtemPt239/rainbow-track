2b366035fc67a57d095eba5f9d956afe19293a44
ArtemPt239 <artem99petrov@phystech.edu>
2022-08-22 02:51:59 +0400

    Rewritten the code in python, added smooth ticking feature, added offline indicator, added instructions to the README

--- README.md
+++ README.md
@@ -1,13 +1,97 @@
 # Rainbow track
-GNOME extension for smooth two-click time tracking with Toggl Track
+GNOME extension for smooth two-click time tracking with Toggl Track.
 
 Built on top of [Argos](https://github.com/p-e-w/argos).
 
-TODO: beautiful gif
+# Features
 
-# Installing
+1) Display your current timer in the GNOME topbar
+
+<img src="img/timer.gif" width="400"/>
+
+2) Stop your current timer, or start a new one with just two clicks
+
+<img src="img/stop_start_timer.gif" width="300"/>
+
+3) "No connection" indicator
+
+<img src="img/offline_indicator.png" width="300"/>
+
+Known limitations:
+
+In the current version it is impossible to set tags or a description when starting a new timer. The only configurable 
+property of the new timer is its project.
+
+# Installation
+
 ### 1) Install Argos.
-Follow the [link](https://github.com/p-e-w/argos) and install it following the [instructions](https://github.com/p-e-w/argos#installation).
+Install Argos following the [instructions](https://github.com/p-e-w/argos#installation).
 
-### 2) Installing Rainbow track
-TODO: instructions
+### 2) Install dependencies
+
+Rainbow Track requires python3 to run, with the `requests` module installed
+
+1) Make sure that you have `python3` and `pip` installed
+
+2) run `pip install requests`
+
+Additionally, make sure, that you have `curl` installed.
+You can check it by running `curl -V` in the terminal.
+
+
+### 3) Installing Rainbow track
+
+1) Clone this repository. One possible location to clone this repository to is `~/.config/argos/`, however, you can clone it wherever you want
+
+```
+cd ~/.config/argos/
+git clone "https://github.com/ArtemPt239/rainbow-track"
+```
+
+2) Create config:
+   1) Create config file
+   ```
+   cd PATH_TO_THE_CLONNED_REPO
+   cp config.default config
+   ```
+   2) [Obtain](#Obtaining-Toggl-Track-API-token-and-workplace-id) Toggl Track API token and workplace id
+   
+   3) Add them to the config file created at the step 2.i
+
+3) Create symlink from Argos directory (located at `~/.config/argos`) to the rainbow-track.py:
+```
+cd ~/.config/argos
+ln -s PATH_TO_THE_CLONNED_REPO/rainbow-track.py rainbow-track.c.1s.py
+```
+Note: it is important to specify this exact name for the symlink, because Argos uses this name to determine update 
+frequency and position of the extension button on the topbar.
+Alternatively, you can name the symlink `rainbow-track.l.1s.py` to place the extension button on the left side of the 
+topbar, or `rainbow-track.r.1s.py` to place the extension button on the right side of the topbar.
+
+# Obtaining Toggl Track API token and workplace id:
+
+### Obtaining Toggl Track API token
+
+1) Go to https://track.toggl.com/profile
+2) Scroll to the bottom of the page
+
+![](img/api_token.png)
+
+3) Click "click to reveal" and copy your API token
+
+### Obtaining workplace id:
+
+1) Go to bottom of the sidebar and click workspace icon
+2) In the opened popup click "Manage Workspaces"
+
+![](img/workplace_id_1-2.png)
+
+3) Click the name of your personal workspace
+
+![](img/workplace_id_3.png)
+
+4) Workspace id will be a part of the opened url. Copy it
+
+![](img/workplace_id_4.png)
+
+
---
+++ rainbow_track.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+import json
+import os
+
+API_URL = 'https://api.track.toggl.com/api/v9'
+CONFIG_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+number_of_runs_till_sync = 5
+number_of_runs_till_projects_sync = 60*60*12
+
+CONNECTION_STATUS_OK = 'ok'
+
+
+with open(f"{CONFIG_DIR_PATH}/config") as config_file:
+    config_json = json.load(config_file)
+    API_TOKEN = str(config_json["token"])
+    WORKPLACE_ID = int(config_json["workplace_id"])
+
+
+def get_persistent_variable(file_name: str, default_value=None):
+    try:
+        with open(f"{CONFIG_DIR_PATH}/var/{file_name}") as variable_file:
+            return variable_file.read()
+    except FileNotFoundError:
+        return default_value
+
+
+def save_persistent_variable(value, file_name: str):
+    with open(f"{CONFIG_DIR_PATH}/var/{file_name}", 'w') as variable_file:
+        variable_file.write(str(value))
+
+
+def build_output():
+    """ Prints output, used by Argos to construct the extension"""
+
+    def paint_text(text: str, hex_color: str) -> str:
+        # Extra empty span is required for color to work properly in older GNOME version
+        # See https://github.com/p-e-w/argos/issues/112#issuecomment-611539550
+        return f'<span> </span><span foreground="{hex_color}">{text}</span>'
+
+    from datetime import datetime
+
+    projects_json_string = get_persistent_variable('projects')
+    if projects_json_string is None:
+        sync_projects()
+        projects_json_string = get_persistent_variable('projects')
+    projects = json.loads(projects_json_string)
+
+    connection_status = ':red_circle:' if get_persistent_variable('connection_status') != CONNECTION_STATUS_OK else ''
+
+    current_time_entry = json.loads(get_persistent_variable('current_time_entry'))
+    if current_time_entry is not None:
+        description = current_time_entry['description']
+        description_string = f"({description})" if not (description is None or description == "") else ""
+
+        project = projects[str(current_time_entry['pid'])]
+        project_color = project['hex_color']
+        project_name = project['name']
+
+        duration_in_seconds = int(datetime.now().timestamp() + current_time_entry['duration'])
+        duration_string = f'{duration_in_seconds // 3600 :d}:{(duration_in_seconds % 3600) // 60 :02d}:{duration_in_seconds % 60 :02d}'
+
+        print(f"{connection_status}{paint_text(f'{project_name} {duration_string}', project_color)} {description_string}")
+
+        print("---")
+
+        current_datetime = datetime.now().astimezone().replace(microsecond=0)
+        print((f"{paint_text('Stop timer', '#ff0000')} | "
+               f"bash='curl -v -u {API_TOKEN}:api_token"
+               f" -H \"Content-Type: application/json\""
+               f" -d \"{{\\\"id\\\":{current_time_entry['id']},\\\"duration\\\":{int(current_time_entry['duration'] + current_datetime.timestamp())},\\\"stop\\\":\\\"{current_datetime.isoformat()}\\\",\\\"wid\\\":{WORKPLACE_ID}}}\""
+               f" -X PUT {API_URL}/time_entries/{current_time_entry['id']} ; python3 {CONFIG_DIR_PATH}/sync_current_timer.py'"
+               f" terminal=false refresh=true"))
+    else:
+        print(f"{connection_status} Timer stopped")
+
+    print("---")
+
+    for pid in projects.keys():
+        current_datetime = datetime.now().astimezone().replace(microsecond=0)
+        print((f"{paint_text(projects[pid]['name'], projects[pid]['hex_color'])} | "
+               f"bash='curl -v -u {API_TOKEN}:api_token"
+               f" -H \"Content-Type: application/json\""
+               f" -d \"{{\\\"pid\\\":{pid},\\\"created_with\\\":\\\"rainbow track\\\",\\\"start\\\":\\\"{current_datetime.isoformat()}\\\",\\\"stop\\\":null,\\\"duration\\\":{int(-current_datetime.timestamp())},\\\"wid\\\":{WORKPLACE_ID}}}\""
+               f" -X POST {API_URL}/time_entries ; python3 {CONFIG_DIR_PATH}/sync_current_timer.py'"
+               f" terminal=false refresh=true"))
+
+
+def make_api_request(resource_location: str):
+    import requests
+    from base64 import b64encode
+
+    authorization_header = 'Basic %s' % b64encode(f"{API_TOKEN}:api_token".encode('ascii')).decode("ascii")
+    headers = {"Content-Type": "application/json", 'Authorization': authorization_header}
+    data = requests.get(f'{API_URL}/{resource_location}', headers=headers)
+    return data
+
+
+def check_if_connection_is_ok(function):
+    import requests
+    def wrapper(*args, **kwargs):
+        try:
+            result = function(*args, **kwargs)
+            save_persistent_variable(CONNECTION_STATUS_OK, 'connection_status')
+            return result
+        except requests.exceptions.ConnectionError as e:
+            save_persistent_variable(e, 'connection_status')
+    return wrapper
+
+
+@check_if_connection_is_ok
+def sync_current_timer():
+    """ Syncs current timer with the toggl api"""
+    save_persistent_variable(json.dumps(make_api_request('me/time_entries/current').json()), 'current_time_entry')
+
+
+@check_if_connection_is_ok
+def sync_projects():
+    """ Syncs list of projects with the toggl api"""
+    projects = {project['id']: {'name': project['name'], 'hex_color': project['color']}
+                for project in sorted(make_api_request('me/projects').json(), key=lambda x: x['name'])}
+    save_persistent_variable(json.dumps(projects, indent=4), 'projects')
+
+
+def main():
+    run_number = int(get_persistent_variable('run_number', default_value=1))
+
+    if run_number % number_of_runs_till_projects_sync == 0:
+        run_number = 0
+        sync_projects()
+
+    if run_number % number_of_runs_till_sync == 0:
+        sync_current_timer()
+
+    save_persistent_variable(run_number + 1, 'run_number')
+    build_output()
+
+
+if __name__ == '__main__':
+    main()
